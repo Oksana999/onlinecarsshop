73e9d0fe60b22626951a1e8e69c488a54c0b629e
Oksana999 <oksanam999@gmail.com>
2019-09-25 11:47:05 -0500

    form for adding new car

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,31 +1,31 @@
+function addCar(carDiv, car) {
+    var textModel = document.createTextNode(car.model);
+    var elementModel = document.createElement("div")
+    elementModel.appendChild(textModel)
+    carDiv.appendChild(elementModel)
+
+    var textMark = document.createTextNode(car.mark);
+    var elementMark = document.createElement("div")
+    elementMark.appendChild(textMark)
+    carDiv.appendChild(elementMark)
+
+    var elementImageName = document.createElement("img")
+    elementImageName.setAttribute("src", car.imageName)
+    elementImageName.setAttribute("width", "300px");
+    carDiv.appendChild(elementImageName)
+
+    var textBuildYear = document.createTextNode(car.buildYear);
+    var elementBuildYear = document.createElement("div")
+    elementBuildYear.appendChild(textBuildYear)
+    carDiv.appendChild(elementBuildYear)
+
+    var textCarPrice = document.createTextNode(car.carPrice);
+    var elementCarPrice = document.createElement("div")
+    elementCarPrice.appendChild(textCarPrice)
+    carDiv.appendChild(elementCarPrice)
+}
+
 function main() {
-    function addCar(carDiv, car) {
-        var textModel = document.createTextNode(car.model);
-        var elementModel = document.createElement("div")
-        elementModel.appendChild(textModel)
-        carDiv.appendChild(elementModel)
-
-        var textMark = document.createTextNode(car.mark);
-        var elementMark = document.createElement("div")
-        elementMark.appendChild(textMark)
-        carDiv.appendChild(elementMark)
-
-        var elementImageName = document.createElement("img")
-        elementImageName.setAttribute("src", car.imageName)
-        elementImageName.setAttribute("width", "300px");
-        carDiv.appendChild(elementImageName)
-
-        var textBuildYear = document.createTextNode(car.buildYear);
-        var elementBuildYear = document.createElement("div")
-        elementBuildYear.appendChild(textBuildYear)
-        carDiv.appendChild(elementBuildYear)
-
-        var textCarPrice = document.createTextNode(car.carPrice);
-        var elementCarPrice = document.createElement("div")
-        elementCarPrice.appendChild(textCarPrice)
-        carDiv.appendChild(elementCarPrice)
-    }
-
     var cars = [
         {
             model: "BMW",
@@ -51,10 +51,30 @@ function main() {
     ];
     var carsDiv = document.getElementById('cars')
     var i;
-    for(i = 0; i < cars.length; i++){
+    for (i = 0; i < cars.length; i++) {
         var car = cars[i];
         var carDiv = document.createElement("div");
         addCar(carDiv, car);
         carsDiv.appendChild(carDiv)
     }
+}
+
+function formAddCarOnClick() {
+    var carsDiv = document.getElementById('cars')
+    var model = document.getElementById('carModelForm').value;
+    var mark = document.getElementById('carMarkForm').value;
+    var buildYear = document.getElementById('carBuildYear').value;
+    var carPrice = document.getElementById('carPrice').value;
+    var imageName = document.getElementById('imageName').value;
+
+    var car = {
+        model: model,
+        mark: mark,
+        buildYear: buildYear,
+        carPrice: carPrice,
+        imageName: imageName
+    };
+    var carDiv = document.createElement("div");
+    addCar(carDiv, car);
+    carsDiv.appendChild(carDiv)
 }
