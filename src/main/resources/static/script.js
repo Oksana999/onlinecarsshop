3e8b4eb66b967df1497880ca2f474f18facea6fc
Oksana999 <oksanam999@gmail.com>
2019-09-25 13:02:07 -0500

    load from localStorage or default cars

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -25,30 +25,48 @@ function addCar(carDiv, car) {
     carDiv.appendChild(elementCarPrice)
 }
 
+function loadCars() {
+    var existedCars = localStorage.getItem("cars");
+    if(existedCars){
+        return JSON.parse(existedCars);
+    }else{
+        return  [
+            {
+                model: "BMW",
+                mark: "XT5",
+                buildYear: 2005,
+                carPrice: 50000,
+                imageName: "https://www.cstatic-images.com/stock/900x600/235994.jpg"
+            }, {
+                model: "BMW",
+                mark: "XT5",
+                buildYear: 2005,
+                carPrice: 50000,
+                imageName: "https://www.cstatic-images.com/stock/900x600/235994.jpg"
+            },
+            {
+                model: "Mercedes-Benz",
+                mark: "W213",
+                buildYear: 2019,
+                carPrice: 55000,
+                imageName: "https://upload.wikimedia.org/wikipedia/commons/9/9b/2019_Mercedes-Benz_E220d_SE_Automatic_2.0_Front.jpg"
+            },
+            {
+                model: "Audi",
+                mark: "A8",
+                buildYear: 2018,
+                carPrice: 30000,
+                imageName: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Audi_A8_D5_IMG_0066.jpg"
+            }
+        ];
+
+    }
+
+}
+
+
 function main() {
-    var cars = [
-        {
-            model: "BMW",
-            mark: "XT5",
-            buildYear: 2005,
-            carPrice: 50000,
-            imageName: "https://www.cstatic-images.com/stock/900x600/235994.jpg"
-        },
-        {
-            model: "Mercedes-Benz",
-            mark: "W213",
-            buildYear: 2019,
-            carPrice: 55000,
-            imageName: "https://upload.wikimedia.org/wikipedia/commons/9/9b/2019_Mercedes-Benz_E220d_SE_Automatic_2.0_Front.jpg"
-        },
-        {
-            model: "Audi",
-            mark: "A8",
-            buildYear: 2018,
-            carPrice: 30000,
-            imageName: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Audi_A8_D5_IMG_0066.jpg"
-        }
-    ];
+    var cars = loadCars();
     var carsDiv = document.getElementById('cars')
     var i;
     for (i = 0; i < cars.length; i++) {
